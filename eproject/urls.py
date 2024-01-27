298974bb9e1de1073566b26442f71aff7ee36e09
dubeykishan1910 <dubeykishan1910@gmail.com>
2024-01-27 22:47:27 +0530

    stu

--- eproject/urls.py
+++ eproject/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path
 from django.conf.urls.static import static 
 from django.conf import settings
 from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
